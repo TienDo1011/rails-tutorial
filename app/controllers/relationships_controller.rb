0b619a734ef9b932b6934e1d5d35a32d1a625ab5
Tien Do <tiendo1011@gmail.com>
2018-06-22 21:07:14 +0700

    Only need user id to follow & unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(@user)
+    current_user.follow!(@user.id)
     render json: {message: "You have followed #{@user.name}!"}
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
+    current_user.unfollow!(@user.id)
     render json: {message: "You have unfollowed #{@user.name}"}
   end
 end
