838712452f040b4c497a8a679d4b1e195b6081fe
Tien Do <tiendo1011@gmail.com>
2018-11-19 22:05:50 +0700

    Add user_name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180923021839) do
+ActiveRecord::Schema.define(version: 20181119150107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180923021839) do
     t.string   "password_digest"
     t.string   "digest_token"
     t.boolean  "admin",           default: false
+    t.string   "user_name"
   end
 
   add_index "users", ["digest_token"], name: "index_users_on_digest_token", using: :btree
