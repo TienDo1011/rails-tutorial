e4ae6278a5308679ffe5f830e33cb44241ca7535
Tien Do <tiendo1011@gmail.com>
2018-12-09 21:25:43 +0700

    Serialize more data for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :admin
+  has_many :microposts
+  has_many :followers
+  has_many :followed_users, key: :followings
+  has_one :user_configuration, key: :settings, serializer: UserConfigurationSerializer
 end
